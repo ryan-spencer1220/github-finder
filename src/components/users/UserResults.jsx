02c8060254aaa0ce93c96ffd5b2945844ec90946
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-05-17 20:54:51 -0700

    Add user search functionality

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,24 +1,43 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState([true]);
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+    const response = await fetch(`https://api.github.com/users`, {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        Authorization: `token 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`,
       },
     });
 
     const data = await response.json();
 
-    console.log(data);
+    setUsers(data);
+    setLoading(false);
   };
 
-  return <div>User Results</div>;
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <h3>
+        <Spinner />
+      </h3>
+    );
+  }
 }
 
 export default UserResults;
