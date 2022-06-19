23073e165b4714dfd492403cee10fd8c0871c4ae
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-06-18 17:18:00 -0700

    Add AlertProvider

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,16 @@
+import { createContext, useReducer } from "react";
+import alertReducer from "./AlertReducer";
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+  const initalState = null;
+
+  const [state, dispatch] = useReducer(alertReducer, initalState);
+
+  return (
+    <AlertContext.Provider value={{ alert: state }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
